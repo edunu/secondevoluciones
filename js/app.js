79db23cd645aaf2f6466c4ef598645c160551a86
Eduardo Calvillo <eduwucp@hotmail.com>
2021-12-02 15:07:34 -0600

    Septimo commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //obtenemos la url del servidor
 var url=window.location.href;
 //definimos que nuestro sw.js se encuentra en el repositorio
-var ubicacionSw='../sw.js';
+var ubicacionSw='/sw.js';
 if ( navigator.serviceWorker ) {
 /*Para que nuestro proyecto siga funcionando en localhost
 como en el servidor realizaremos una validación si la url
